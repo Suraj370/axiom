8fdaa6640997a753ba5c84939d544e74dedfc5fb
Suraj370 <surajpanda2077@gmail.com>
2025-10-17 10:00:26 +0530

    feat: Implement server-backed data fetching and enhance performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
-import Image from "next/image";
+// import { caller } from "@/trpc/server";
+import { getQueryClient, trpc } from "@/trpc/server";
+import Client from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+export default async function Home() {
+  const queryClient = getQueryClient();
 
-export default function Home() {
-  return <main>Home!</main>;
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
+  return (
+    <main>
+      <h1>Home!</h1>
+      <ul>
+        <HydrationBoundary state = {dehydrate(queryClient)}>
+          <Suspense fallback = {<p>Loading...</p>}>
+            <Client />
+          </Suspense>
+        </HydrationBoundary>
+      </ul>
+    </main>
+  );
 }
