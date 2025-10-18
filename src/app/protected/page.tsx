c33aa84a102f041b8fc1409fc954498a265edb45
Suraj370 <surajpanda2077@gmail.com>
2025-10-18 11:49:44 +0530

    feat: add Inngest integration and workflow management
    - Added Inngest client and helloWorld function for asynchronous processing.
    - Introduced Workflow model in Prisma schema and corresponding database migrations.
    - Implemented TRPC routes for fetching and creating workflows.
    - Updated protected page to utilize TRPC for fetching workflows and creating new ones.
    - Removed obsolete TRPC route and client components.
    - Enhanced layout to include Toaster for notifications.

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,20 +1,36 @@
-import Logout from '@/components/Logout';
-import { Button } from '@/components/ui/button';
-import { requireAuth } from '@/lib/auth-utils';
-import { caller } from '@/trpc/server';
-import { LogOut } from 'lucide-react';
-import React from 'react'
+"use client";
 
-async function page() {
-  const session = await requireAuth();
 
-  const data = await caller.getUsers();
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import Logout from "@/components/Logout";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+const Page = () => {
+
+  const trpc = useTRPC();
+
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const createWorkflow = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      // queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      toast.success("Job queued")
+
+    },
+  }));
+
   return (
-    <div className='min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6'>
-        <div>{JSON.stringify(data, null, 2)}</div>
-       <Logout />
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      protected server component
+      <div>
+        {JSON.stringify(data, null, 2)}
+      </div>
+      <Button disabled={createWorkflow.isPending} onClick={() => createWorkflow.mutate()}>Create Workflow</Button>
+      <Logout />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,28 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
-import prisma from '@/lib/db';
+import { inngest } from "@/inngest/client";
+import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure
+  getUsers: protectedProcedure.query(({ ctx }) => {
+    return prisma.user.findMany({
+      where: {
+        id: ctx.auth.user.id,
+      },
+    });
+  }),
 
-    .query(({ ctx }) => {
-      
-      return prisma.user.findMany({
-        where: {
-          id: ctx.auth.user.id
-        }
-      });
-    }),
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+
+  createWorkflow: protectedProcedure.mutation(async({ ctx }) => {
+    await inngest.send({
+      name: 'test/hello.world',
+      data: {
+        email: ctx.auth.user.email,
+      },
+    })
+    return {success: true, message: "Job queue"}
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
